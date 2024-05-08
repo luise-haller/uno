fcd1f96c3e3ae092236d0099b462465f2fd1d200
Andrew Cobar <100027419@mvla.net>
2024-05-08 12:23:24 -0700

    Update ServerThread.java

--- ServerThread.java
+++ ServerThread.java
@@ -1,7 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ServerThread implements Runnable {
+    private Socket clientSocket;
+    private Manager m;
+    private PrintWriter out = null;
+
+
     
+    public void send(String message) {
+        out.println(message);
+    }
 
     public void run() {
         
+        try {
+            // System.out.println(Thread.currentThread().getName() + ": connection
+            // opened.");
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            
+
+            while (true) {
+                
+            }
+        } catch (IOException ex) {
+            System.out.println("Error listening for a connection");
+            System.out.println(ex.getMessage());
+        }
     }
+    
 }
