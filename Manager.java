9ba1c691ab70d28e329a3891cda0bc3ffcdd7c68
Andrew Cobar <100027419@mvla.net>
2024-05-08 14:08:06 -0700

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -11,4 +11,18 @@ public class Manager {
     public void add(ServerThread st) {
         threads.add(st);
     }
+
+    public void startGame() {
+        if (threads.size() >= 3 || threads.size() <= 4) {
+            //Turn Regulation Code Goes Here
+            //Use dealHand() method from Game.java which takes the top 7 cards from the shuffled deck
+        }
+    }
+
+    public void broadcast(String msg) {
+        System.out.println("broadcasting " + msg + " on thread size: " + threads.size());
+        for (int i = 0; i<threads.size();i++) {
+            threads.get(i).send(msg);
+        }
+    }
 }
