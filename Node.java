9572fbba90b68770c0783fb54e5912b38d9fa293
Andrew Cobar <100027419@mvla.net>
2024-05-09 08:55:56 -0700

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,41 @@
+public class Node<E> {
+    private E data;
+    private Node<E> next;
+    private Node<E> prev;
+
+    public Node(E data) {
+        this.data = data;
+        next = null;
+        prev = null;
+    }
+
+    public E get() {
+        return data;
+    }
+
+    public void set(E data) {
+        this.data = data;
+    }
+    
+    public Node<E> next() {
+        return next;
+    }
+
+    public Node<E> prev() {
+        return prev;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public void setPrev(Node<E> prev) {
+        this.prev = prev;
+    }
+    
+
+}
+
+
+
+
