c8c48d4330647cfb362eb005b3507c0434b2ad62
Andrew Cobar <100027419@mvla.net>
2024-05-29 02:09:53 -0700

    Tuesday Night Progress (Andrew)

--- Manager.java
+++ Manager.java
@@ -22,6 +22,37 @@ public class Manager {
         return false;
     }
 
+    public void broadcast(String msg) {
+        System.out.println("broadcasting " + msg + " on thread size: " + threads.size());
+        for (int i = 0; i<threads.size();i++) {
+            threads.get(i).send(msg);
+        }
+    }
+
+    public DLList<Card> getDeck() {
+        return game.getDeckPile();
+    }
+
+    public void startGame() {
+        System.out.println("startGame() has been called");
+        System.out.println("Thread size = " + threads.size());
+        // later fix: 4 players
+        if (threads.size() >= 1) {
+            // Deal hands to players
+            for (int i = 0; i < threads.size(); i++) {
+                ServerThread thread = threads.get(i);
+                DLList<Card> hand = game.dealHand();
+                thread.send("Hand" + hand.toString());    
+            }
+            DLList<Card> deck = game.getDeckPile();
+            Card topCard = deck.get(0);
+            broadcast("Top" + topCard.toString());
+        } else {
+            System.out.println("Game requires 3 to 4 players to start.");
+        }
+    }
+
+
     // idk if what i wrote here makes sense LOL - > created new one below!
     // public void regulate(boolean reversed) {
     //     if(!reversed) {
@@ -43,6 +74,9 @@ public class Manager {
     //     }
         
     // }
+
+    
+
     public void regulate(boolean reversed) {
         // if reversed is true - > set currentIndex to last index
         // if reversed is false - > set currentIndex to 0 (first index)
@@ -65,42 +99,25 @@ public class Manager {
         currentIndex = (currentIndex + step + threads.size()) % threads.size();
     }
     
+    //Called in ServerThread which takes and removes top card from deck and adds to one client's hand
     public Card drawCardFromDeck() {
         Card draw = game.getDeckPile().get(0);
         game.getDeckPile().remove(0);
         return draw;
     }
 
+    //Broadcast the new top card to ALL Clients
     public void updateCardInPlay(String newCard) {
         broadcast("Top" + newCard);
-        //broadcast the new card to ALL clients
     }
 
-    public DLList<Card> getDeck() {
-        return game.getDeckPile();
-    }
-    public void startGame() {
-        System.out.println("Thread size = " + threads.size());
-        // later fix: 4 players
-        if (threads.size() == 1) {
-            // Deal hands to players
-            for (int i = 0; i < threads.size(); i++) {
-                ServerThread thread = threads.get(i);
-                DLList<Card> hand = game.dealHand();
-                thread.send("Hand" + hand.toString());    
-            }
-            DLList<Card> deck = game.getDeckPile();
-            Card topCard = deck.get(0);
-            broadcast("Top" + topCard.toString());
-        } else {
-            System.out.println("Game requires 3 to 4 players to start.");
-        }
-    }
-
-    public void broadcast(String msg) {
-        System.out.println("broadcasting " + msg + " on thread size: " + threads.size());
-        for (int i = 0; i<threads.size();i++) {
-            threads.get(i).send(msg);
-        }
+    //Logic for draw 2 action card
+    public DLList<Card> draw2() {
+        DLList<Card> newCards = new DLList<Card>();
+        Card c1 = game.getDeckPile().get(0);
+        Card c2 = game.getDeckPile().get(1);
+        game.getDeckPile().remove(0);game.getDeckPile().remove(0);
+        newCards.add(c1);newCards.add(c2);
+        return newCards;
     }
 }
