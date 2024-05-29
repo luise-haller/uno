8e0b94b48e7eec45b9ec3f4a2a0a1ec49d055fe3
Andrew Cobar <100027419@mvla.net>
2024-05-29 02:09:28 -0700

    Tuesday Night Progress (Andrew)

--- ClientScreen.java
+++ ClientScreen.java
@@ -20,18 +20,18 @@ import java.io.*;
 import java.net.*;
 import java.awt.*;
 
-
 public class ClientScreen extends JPanel implements ActionListener, MouseListener {
-    private BufferedReader in;
     private PrintWriter out;
+    private BufferedReader in;
 
     private BufferedImage background, logo;
     private DLList<Card> myHand;
-    private Card cardInPlay;
-    private Card cardSelected;
+    private Card cardInPlay, cardSelected;
 
-    private boolean myTurn, gameStarted, displayRules;
     private String hostName, username, playerName;
+    private boolean myTurn, gameStarted, displayRules;
+    private boolean userIn = false;
+    private boolean stop;
 
     private JButton startGameButton, submitButton, rulesButton, doneButton;
     private JTextField ipAddressField, usernameField;
@@ -47,11 +47,9 @@ public class ClientScreen extends JPanel implements ActionListener, MouseListene
         this.setFocusable(true);
         addMouseListener(this);
 
+        //Instantiate this client's hand
         myHand = new DLList<Card>();
-        
-        cardInPlay = null; cardSelected = null;
-        playerName = null;
-
+        cardInPlay = null; cardSelected = null; 
         myTurn = false;
         gameStarted = false;
         displayRules = false;
@@ -109,6 +107,68 @@ public class ClientScreen extends JPanel implements ActionListener, MouseListene
 			event.printStackTrace();
 		}
     }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == startGameButton) {
+            startGameButton.setVisible(false);
+            ipAddressField.setVisible(true);
+            usernameField.setVisible(true);
+            submitButton.setVisible(true);
+        } else if (e.getSource() == submitButton) {
+            this.hostName = ipAddressField.getText();
+            this.username = usernameField.getText();
+            this.playerName = usernameField.getText();
+            this.gameStarted = true;
+        } else if (e.getSource() == rulesButton) {
+            this.displayRules = true;
+        } else if (e.getSource() == doneButton) {
+            this.displayRules = false;
+            doneButton.setVisible(false);
+        }
+        repaint();
+    }
+
+    public void connect() throws IOException {
+        int portNumber = 1024;
+        Socket serverSocket = new Socket(hostName, portNumber);
+        in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+        out = new PrintWriter(serverSocket.getOutputStream(), true);
+        String msg = "";
+        try {
+            out.println(username);   //sends username to ServerThread
+            out.println("play");   //this message is sent to ServerThread in order to call manager.startGame
+            while (true) {
+                msg = in.readLine();
+                System.out.println("Message from Server\n" + msg);
+                if(gameStarted) {
+                    processServerMessage(msg); //See processServerNessage private method
+                }
+                repaint();
+            }
+            
+            
+            
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
+            System.exit(1);
+        }
+    }
+
+
+    private void processServerMessage(String msg) {
+        if (msg.substring(0, 4).equals("Hand")) { 
+            myHand = transformHand(msg.substring(4));
+        } else if (msg.substring(0, 3).equals("Top")) {
+            cardInPlay = transformCard(msg.substring(3));
+        } else if (msg.equals("Your Turn")) {
+            this.myTurn = true;
+        } else if (msg.startsWith("Username")) {
+            this.playerName = msg.substring(8);
+        } else if (msg.startsWith("FromDraw")) {
+            myHand.add(transformCard(msg.substring(8)));
+        }
+    }
+
     public Dimension getPreferredSize() {
         return new Dimension(800, 600);
     }
@@ -133,9 +193,6 @@ public class ClientScreen extends JPanel implements ActionListener, MouseListene
                 g.drawImage(logo, 465, 130, 70, 70, null);
                 drawCard(g, cardInPlay, 270, 100);
 
-            
-                // System.out.println("First Card in my hand: " + myHand.get(0).toString());
-                // drawCard(g, myHand.get(0), 100, 500);
                 if(myHand.size() >= 0) {
                     for (int i = 0; i<myHand.size();i++) {
                         drawCard(g, myHand.get(i), 100 + i*55, 400);
@@ -164,128 +221,68 @@ public class ClientScreen extends JPanel implements ActionListener, MouseListene
             
         }
     }
-    private void drawRulebook(Graphics g) {
-        g.setColor(new Color(34, 82, 52));
-        g.fillRect(50, 50, 700, 500);
-        g.setColor(Color.BLACK);
-        g.setFont(new Font("Arial", Font.BOLD, 24));
-        g.drawString("Rulebook", 350, 100);
 
-        String[] rules = {
-            "1. Each player is dealt 7 cards.",
-            "2. The top card of the deck is placed face up to start the discard pile.",
-            "3. Players take turns matching a card from their hand with the current card.",
-            "4. Cards can be matched by number, color, or symbol.",
-            "5. Special cards can change the flow of the game.",
-            "6. If a player cannot match a card, they must draw a card from the deck.",
-            "7. The first player to get rid of all their cards wins."
-        };
-
-        g.setFont(new Font("Arial", Font.PLAIN, 18));
-        int yPosition = 150;
-        for (String rule : rules) {
-            g.drawString(rule, 100, yPosition);
-            yPosition += 30;
+    public void mousePressed(MouseEvent e) {        
+        if (myTurn) {
+            if(myHand.size() >= 0) { // later add: if more than 20 hand cards, client automatically disconnected
+                for (int i = 0; i<myHand.size();i++) {
+                    if (e.getY() > 400 && e.getY() < 550) {
+                        int last = myHand.size()-1;
+                        if (i == last) {
+                            if (e.getX() >= 100 +i*55 && e.getX() < 200 + i*55) {
+                                cardSelected = myHand.get(i);
+                                
+                            }    
+                        } else {
+                            if (e.getX() >= 100 +i*55 && e.getX() < 155 + i*55) {
+                                cardSelected = myHand.get(i);
+                                
+                            }
+                        }
+                    }
+                }
+            } 
+            if (myHand.size() > 20) { // test this out
+                System.out.println("You have more than 20 cards! You are being kicked out of the game.");
+                return;
+            }
+            // If clicked on draw pile
+            if (e.getX() >= 450 && e.getX() <= 550 && e.getY() >= 100 && e.getY() <= 250) {
+                out.println("DrawCard");
+            }
+            if (cardSelected != null) {
+                if (this.myTurn && canPlayCard(cardSelected, cardInPlay)) {
+                    playCardFromHand(cardSelected);
+                }
+            }
+            myTurn = false;
+            out.println("ClientWent");
         }
+        
     }
-    
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == startGameButton) {
-            startGameButton.setVisible(false);
-            ipAddressField.setVisible(true);
-            usernameField.setVisible(true);
-            submitButton.setVisible(true);
-        } else if (e.getSource() == submitButton) {
-            this.hostName = ipAddressField.getText();
-            this.username = usernameField.getText();
-            this.gameStarted = true;
-        } else if (e.getSource() == rulesButton) {
-            this.displayRules = true;
-        } else if (e.getSource() == doneButton) {
-            this.displayRules = false;
-            doneButton.setVisible(false);
-        }
+    //Private Methods to Play a card from hand and replace cardInPlay
+    private boolean canPlayCard(Card selected, Card inPlay) {
+        return selected.getColor().equals(inPlay.getColor()) || selected.getValue().equals(inPlay.getValue());
+    }
+    private void playCardFromHand(Card selected) {
+        this.cardInPlay = selected;
+        //Send cardInPlay to server thread
+        out.println("ChangeCard" + cardInPlay.toString());
+        myHand.remove(selected);
+        cardSelected = null;
+        
         repaint();
     }
+
     public boolean getGameStarted() {
         return this.gameStarted;
     }
-    public void connect() throws IOException {
-        Scanner sc = new Scanner(System.in);
 
-        // When client clicks play, they are asked to enter id address of server & their player name
-        // Then will be connected & "enter" the game
-        int portNumber = 1024;
-        // System.out.println("host name" + hostName);
-        Socket serverSocket = new Socket(hostName, portNumber);
-        out = new PrintWriter(serverSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
-
-        try {
-            out.println("Username" + username);
-            while(true) {
-                
-                String msg = in.readLine();
-                if (msg == null) {
-                    System.err.println("Received null message from server. Exiting...");
-                    break;
-                }
-                processServerMessage(msg);
-                repaint();
-            }
-        } catch(IOException e) {
-            System.err.println("Couldn't get I/O for the connection");
-            System.exit(1);
-        }
-        
     
-       
-    }
-    private void processServerMessage(String msg) {
-        if (msg.substring(0, 4).equals("Hand")) {
-            myHand = transformHand(msg.substring(4));
-        } else if (msg.substring(0, 3).equals("Top")) {
-            cardInPlay = transformCard(msg.substring(3));
-        } else if (msg.equals("Your Turn")) {
-            this.myTurn = true;
-        } else if (msg.startsWith("Username")) {
-            this.playerName = msg.substring(8);
-        } else if (msg.startsWith("CardDrawn")) {
-            myHand.add(transformCard(msg.substring(9)));
-        }
-        // else if (msg.startsWith("DeckFromGame")) {
-        //     String stringDeck = msg.substring(12);
-        //     deck = transformHand(stringDeck);
-        //     System.out.println("DECK" + deck);
-        // }
-    }
-
-    private DLList<Card> transformHand(String s) {
-        System.out.println(s);
-        String[] array = s.split(",");
-        DLList<Card> hand = new DLList<Card>();
-        for (int i = 0; i< array.length; i++) {
-            String[] split = array[i].split(" ");
-            String color = split[0];
-            String value = split[1];
-            hand.add(new Card(color, value));
-        }
-        return hand;
-    }
-
-    private Card transformCard(String s) {
-        String[] nS = s.split(" ");
-        String color = nS[0];
-        String value = nS[1];
-        Card card = new Card(color, value);
-        return card;
-    }
-    
-    
+    //Draws card Gaphically
     private void drawCard(Graphics g, Card card, int x, int y) {
         //take in the value and color of the card
         //draws card graphically
-        // System.out.println(card.toString());
         if(card.getColor().equals("Yellow")) {
             g.setColor(Yellow);
         } else if(card.getColor().equals("Blue")) {
@@ -324,59 +321,55 @@ public class ClientScreen extends JPanel implements ActionListener, MouseListene
         g.drawImage(logo, x+15, y+30, 70, 70, null);
     }
 
-    public void mousePressed(MouseEvent e) {        
-        
-        if(myHand.size() >= 0) { // later add: if more than 20 hand cards, client automatically disconnected
-            for (int i = 0; i<myHand.size();i++) {
-                if (e.getY() > 400 && e.getY() < 550) {
-                    int last = myHand.size()-1;
-                    if (i == last) {
-                        if (e.getX() >= 100 +i*55 && e.getX() < 200 + i*55) {
-                            cardSelected = myHand.get(i);
-                        }    
-                    } else {
-                        if (e.getX() >= 100 +i*55 && e.getX() < 155 + i*55) {
-                            cardSelected = myHand.get(i);
-                        }
-                    }
-                }
-            }
-        } 
-        if (myHand.size() > 20) { // test this out
-            System.out.println("You have more than 20 cards! You are being kicked out of the game.");
-            return;
+    
+    private DLList<Card> transformHand(String s) {
+        String[] array = s.split(",");
+        DLList<Card> hand = new DLList<Card>();
+        for (int i = 0; i< array.length; i++) {
+            String[] split = array[i].split(" ");
+            String color = split[0];
+            String value = split[1];
+            hand.add(new Card(color, value));
         }
-        // If clicked on draw pile
-        if (e.getX() >= 450 && e.getX() <= 550 && e.getY() >= 100 && e.getY() <= 250) {
-            System.out.println("Clicked on draw pile.");
-            out.println("DrawCard");
-            System.out.println("Message DrawCard sent to server");
-        }
-        if (cardSelected != null) {
-            // System.out.println("Card Selected = " + cardSelected.toString());
-            if (this.myTurn && canPlayCard(cardSelected, cardInPlay)) {
-                playCardFromHand(cardSelected);
-                
-            }
-        }
-    }
-    private boolean canPlayCard(Card selected, Card inPlay) {
-        return selected.getColor().equals(inPlay.getColor()) || selected.getValue().equals(inPlay.getValue());
-    }
-    private void playCardFromHand(Card selected) {
-        this.cardInPlay = selected;
-        //Send cardInPlay to server threaad
-        System.out.println("Client Played " + cardInPlay.toString());
-        out.println("ChangeCard" + cardInPlay.toString());
-        myHand.remove(selected);
-        cardSelected = null;
-        
-        repaint();
+        return hand;
     }
 
+    private Card transformCard(String s) {
+        String[] nS = s.split(" ");
+        String color = nS[0];
+        String value = nS[1];
+        Card card = new Card(color, value);
+        return card;
+    }
+
+
+    private void drawRulebook(Graphics g) {
+        g.setColor(new Color(34, 82, 52));
+        g.fillRect(50, 50, 700, 500);
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.BOLD, 24));
+        g.drawString("Rulebook", 350, 100);
+
+        String[] rules = {
+            "1. Each player is dealt 7 cards.",
+            "2. The top card of the deck is placed face up to start the discard pile.",
+            "3. Players take turns matching a card from their hand with the current card.",
+            "4. Cards can be matched by number, color, or symbol.",
+            "5. Special cards can change the flow of the game.",
+            "6. If a player cannot match a card, they must draw a card from the deck.",
+            "7. The first player to get rid of all their cards wins."
+        };
+
+        g.setFont(new Font("Arial", Font.PLAIN, 18));
+        int yPosition = 150;
+        for (String rule : rules) {
+            g.drawString(rule, 100, yPosition);
+            yPosition += 30;
+        }
+    }
+    
     public void mouseReleased(MouseEvent e) {}
     public void mouseEntered(MouseEvent e) {}
     public void mouseExited(MouseEvent e) {}
     public void mouseClicked(MouseEvent e) {}
-
 }
