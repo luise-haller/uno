83629f9bf4ee922226d9da15a73c7f5f1a0c84af
Andrew Cobar <100027419@mvla.net>
2024-05-30 02:26:19 -0700

    Still can't get draw 4 card to work

--- Manager.java
+++ Manager.java
@@ -1,13 +1,15 @@
 public class Manager {
     private Game game;
     private MyArrayList<ServerThread> threads;
-    private boolean nextClientDraw2, skipNextClient;
+    private boolean nextClientDraw2, skipNextClient, reversed, nextClientDraw4;
     
     public Manager() {
         threads = new MyArrayList<ServerThread>();
         game = new Game();
         nextClientDraw2 = false;
+        nextClientDraw4 = false;
         skipNextClient = false;
+        reversed = false;
         
     }
 
@@ -60,39 +62,55 @@ public class Manager {
 
     public void regulate(int currentIndex) {
         int nextClient = -1;
-        if (currentIndex != 3) {
-            nextClient = currentIndex + 1;
-        } else {
-            nextClient = 0;
-        }
-        if(nextClientDraw2) {
-            threads.get(nextClient).send("MustDrawTwo" + draw2().toString());
-            nextClientDraw2 = false;
-        } else if (skipNextClient) {
-            threads.get(nextClient).send("Skipped");
-            if(nextClient!=3) {
-                nextClient+=1;
+        System.out.println("Is Next Client Drawing 4? " + nextClientDraw4);
+        if (!reversed) { //normal order 0 -> 3
+            if (currentIndex != 3) {
+                nextClient = currentIndex + 1;
             } else {
                 nextClient = 0;
             }
-            skipNextClient = false;
-            // System.out.println("Next Client after Skip is Thread#" + nextClient);
+            if(nextClientDraw2) { //Draw 2
+                threads.get(nextClient).send("MustDrawTwo" + draw2().toString());
+                nextClientDraw2 = false;
+            } else if (skipNextClient) { // Skip
+                threads.get(nextClient).send("Skipped");
+                if(nextClient!=3) {
+                    nextClient+=1;
+                } else {
+                    nextClient = 0;
+                }
+                skipNextClient = false;
+            } else if(nextClientDraw4) { // Draw 4
+                System.out.println("Thread #" + currentIndex + " has called Draw 4 on Thread #" + nextClient);
+
+                threads.get(nextClient).send("MustDrawFour" + draw4().toString());
+                nextClientDraw4 = false;
+            }
+        } else { //Reverse Order 3 -> 0
+            if (currentIndex != 0) {
+                nextClient = currentIndex - 1;
+            } else {
+                nextClient = 3;
+            }
+            if(nextClientDraw2) { //Draw 2
+                threads.get(nextClient).send("MustDrawTwo");
+                nextClientDraw2 = false;
+            } else if (skipNextClient) { //Skip
+                threads.get(nextClient).send("Skipped");
+                if(nextClient!=0) {
+                    nextClient-=1;
+                } else {
+                    nextClient = 3;
+                }
+                skipNextClient = false;
+            } else if(nextClientDraw4) { // Draw 4
+                threads.get(nextClient).send("MustDrawFour" + draw4().toString());
+                nextClientDraw4 = false;
+            }
         }
         broadcast("NextClient" + nextClient);
     }
-    public void reverseRegulate(int currentIndex) {
-        int nextClient = 4;
-        if (currentIndex != 0) {
-            nextClient = currentIndex - 1;
-        } else {
-            nextClient = 3;
-        }
-        if(nextClientDraw2) {
-            threads.get(nextClient).send("MustDrawTwo");
-            nextClientDraw2 = false;
-        }
-        broadcast("NextClientReverse" + nextClient);
-    }
+    
     //Called in ServerThread which takes and removes top card from deck and adds to one client's hand
     public Card drawCardFromDeck() {
         Card draw = game.getDeckPile().get(0);
@@ -120,7 +138,8 @@ public class Manager {
         Card c2 = game.getDeckPile().get(1);
         Card c3 = game.getDeckPile().get(2);
         Card c4 = game.getDeckPile().get(3);
-        game.getDeckPile().remove(0);game.getDeckPile().remove(1);game.getDeckPile().remove(2);game.getDeckPile().remove(3);
+        game.getDeckPile().remove(0);game.getDeckPile().remove(0);
+        game.getDeckPile().remove(0);game.getDeckPile().remove(0);
         newCards.add(c1); newCards.add(c2); newCards.add(c3); newCards.add(c4);
         return newCards;
     }
@@ -128,9 +147,23 @@ public class Manager {
     public void clientDraw2(boolean b) {
         this.nextClientDraw2 = b;
     }
+    
+    public void clientDraw4(boolean b) {
+        System.out.println("MANAGER HAS BEEN ACTIVATED");
+        this.nextClientDraw4 = b;
+    }
 
     public void skipNextClient(boolean b) {
         this.skipNextClient = b;
     }
+
+    public void reverseOrder(boolean b) {
+        reversed = b;
+    }
+
+    public boolean isReversed() {
+        return reversed;
+    }
+
 
 }
